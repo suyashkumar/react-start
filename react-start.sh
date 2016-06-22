d058977cd8f5979160ec73da6eaeaa02005bc8aa
Suyash Kumar <suyash.kumar@duke.edu>
2016-06-22 00:35:48 -0400

    Added babelrc and wget statements

--- react-start.sh
+++ react-start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # This script quickly starts up a react project how I like 'em.
+# @author: Suyash Kumar <suyashkumar2003@gmail.com>
 
 npm init
 npm install --save react react-dom && npm install --save-dev html-webpack-plugin webpack webpack-dev-server babel-{core,loader} babel-preset-react
 mkdir app
-mkdir dist
-#TODO: get webpack.config.js from github
-#TODO: get .babelrc
+mkdir dist 
+wget https://raw.githubusercontent.com/suyashkumar/react-start/master/webpack.config.js # get webpack config
+wget https://raw.githubusercontent.com/suyashkumar/react-start/master/.babelrc # get .babelrc
 
