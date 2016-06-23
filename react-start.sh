c314e76d187407a5432d77510cca24183cfc4fc6
Suyash Kumar <suyash.kumar@duke.edu>
2016-06-23 01:54:42 -0400

    Updated babel config and bash script for ES6

--- react-start.sh
+++ react-start.sh
@@ -3,7 +3,7 @@
 # @author: Suyash Kumar <suyashkumar2003@gmail.com>
 
 npm init
-npm install --save react react-dom && npm install --save-dev html-webpack-plugin webpack webpack-dev-server babel-{core,loader} babel-preset-react
+npm install --save react react-dom && npm install --save-dev html-webpack-plugin webpack webpack-dev-server babel-{core,loader} babel-preset-react babel-preset-es2015
 mkdir app
 touch app/index.html
 touch app/index.js
