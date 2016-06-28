ae7877361db0e74e14500292cf9094bd1673d365
Suyash Kumar <suyash.kumar@duke.edu>
2016-06-27 22:38:58 -0400

    Added boilerplate index files

--- react-start.sh
+++ react-start.sh
@@ -4,10 +4,13 @@
 
 npm init
 npm install --save react react-dom && npm install --save-dev html-webpack-plugin webpack webpack-dev-server babel-{core,loader} babel-preset-react babel-preset-es2015
-mkdir app
-touch app/index.html
-touch app/index.js
+mkdir app 
 mkdir dist 
 wget https://raw.githubusercontent.com/suyashkumar/react-start/master/webpack.config.js # get webpack config
 wget https://raw.githubusercontent.com/suyashkumar/react-start/master/.babelrc # get .babelrc
+wget https://raw.githubusercontent.com/suyashkumar/react-start/master/index.html 
+wget https://raw.githubusercontent.com/suyashkumar/react-start/master/index.js
+mv index.html app/index.html
+mv index.js app/index.js
+
 
